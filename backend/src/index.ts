a1f577bcb4458a9e9d8905047442b03a1e601b0a
grim-reaper883 <m.asakib05@gmail.com>
2026-08-28 11:43:59 +0600

    add Clerk integration and environment variable management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,23 @@
-import express from 'express'
+import "dotenv/config"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
+import { getEnv } from "./lib/env";
 
-app.listen(3001, () =>console.log("server running on 3001"))
+const env = getEnv()
+const app = express();
+const rawJson = express.raw({ type: "application/json", limit: "1mb" });
+
+app.post("webhooks/clerk", rawJson, (req, res) => {
+  void clerkWebhookHandler(req, res);
+});
+//clerk data should not be parsed it needs to be in raw json
+
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.listen(env.PORT, () => console.log("server running on:", env.PORT));
