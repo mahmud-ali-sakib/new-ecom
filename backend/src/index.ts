aeb621987fc2e0aff4a375ddd033cdab4a24a21e
grim-reaper883 <m.asakib05@gmail.com>
2026-08-29 12:16:15 +0600

    add checkout functionality with checkout router, controller, and polar integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ import keepAliveCron from "./lib/cron";
 import productRouter from "./routes/productRouter"
 import meRouter from "./routes/meRouter"
 import streamRouter from "./routes/streamRouter"
+import checkoutRouter from "./routes/checkoutRouter"
 
 
 const env = getEnv();
@@ -22,6 +23,9 @@ const rawJson = express.raw({ type: "application/json", limit: "1mb" });
 app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res);
 });
+// app.post("/webhooks/polar", rawJson, (req, res) => {
+//   void polarWebhookHandler(req, res);
+// });
 //clerk data should not be parsed it needs to be in raw json
 
 //middleware
@@ -37,6 +41,7 @@ app.get("/health", (_req, res) => {
 app.use("/api/me", meRouter)
 app.use("/api/products", productRouter)
 app.use("/api/stream", streamRouter)
+app.use("/api/checkout", checkoutRouter)
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
