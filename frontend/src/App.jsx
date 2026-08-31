fe7458922374d4320bb1792a905adfc4dbfb1d58
grim-reaper883 <m.asakib05@gmail.com>
2026-08-31 11:08:56 +0600

    feat: enhance frontend with new components, routing, and image optimization; add backend seeding script

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,17 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
 import PageLoader from './components/PageLoader';
 import Layout from "./components/Layout";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const {isLoaded} = useAuth()
   if(!isLoaded) return <PageLoader/>
   return (
     <Layout>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
     </Layout>
   );
 }
