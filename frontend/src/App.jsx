eb8cbdfbfdf7eb8e2142be1abffd816d22115bf5
grim-reaper883 <m.asakib05@gmail.com>
2026-09-02 11:32:04 +0600

    feat: add OrdersPage, OrderPreview component, and useOrdersPage hook for order management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
 
 function App() {
-  const { isLoaded } = useAuth();
+  const { isLoaded, isSignedIn } = useAuth();
   if (!isLoaded) return <PageLoader />;
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/orders"
+          element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
+        />
       </Routes>
     </Layout>
   );
