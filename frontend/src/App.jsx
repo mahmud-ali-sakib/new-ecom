d7e69bd333ae9f5d4e6371e9bb265978fb61f999
grim-reaper883 <m.asakib05@gmail.com>
2026-09-02 11:20:17 +0600

    feat: implement CartPage with empty state, loading skeletons, and checkout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
-import PageLoader from './components/PageLoader';
+import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
-  const {isLoaded} = useAuth()
-  if(!isLoaded) return <PageLoader/>
+  const { isLoaded } = useAuth();
+  if (!isLoaded) return <PageLoader />;
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </Layout>
   );
