993553f43464a47cdd94f2e5b86053414f5ca408
grim-reaper883 <m.asakib05@gmail.com>
2026-09-02 14:12:56 +0600

    feat: add OrderVideoPage and useOrderVideoPage hook for video support in orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { SentryDemoPage } from './pages/SentryDemoPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import OrderSummaryPage from './pages/OrderSummaryPage';
 import OrderChatPage from './pages/OrderChatPage';
+import OrderVideoPage from "./pages/OrderVideoPage";
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -28,6 +29,11 @@ function App() {
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
 
          <Route path="/demo-sentry" element={<SentryDemoPage />} />
+
+            <Route
+          path="/orders/:id/call"
+          element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />}
+        />
 
 
              {/* NESTED ROUTES */}
